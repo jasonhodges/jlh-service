405daa4ce5eed549f5560a808526e08522761dc4
Jason Hodges <jason@jasonhodges.me>
2015-10-13 21:02:12 -0400

    remove cors

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var app = express();
 
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, Accept, Origin, Referer, User-Agent, Content-Type, Authorization, Content-Length');
 
     // intercept OPTIONS method
     if ('OPTIONS' == req.method) {
