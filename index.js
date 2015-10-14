6461f7081a7c414b791cf35dc259ec47fb160489
Jason Hodges <jason@jasonhodges.me>
2015-10-13 20:54:38 -0400

    add cors

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 var express = require('express');
 var app = express();
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+var cors = require('cors');
 
-    // intercept OPTIONS method
-    if ('OPTIONS' == req.method) {
-      res.send(200);
-    }
-    else {
-      next();
-    }
-};
-app.configure(function(){
-  app.use(allowCrossDomain);
-});
+app.use(cors());
+// var allowCrossDomain = function(req, res, next) {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+//     // intercept OPTIONS method
+//     if ('OPTIONS' == req.method) {
+//       res.send(200);
+//     }
+//     else {
+//       next();
+//     }
+// };
+// app.configure(function(){
+//   app.use(allowCrossDomain);
+// });
 
 
 var db = require('./db.json');
